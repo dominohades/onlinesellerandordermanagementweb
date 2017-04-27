bc1dd0aa27b9e1755b0085cc9240a5f25d3b8995
rigoradriano <rigoradriano@gmail.com>
2017-04-27 16:22:44 +0800

    SampleLogin

---
+++ MvcLoginRegistration/MvcLoginRegistration/Controllers/AccountController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcLoginRegistration.Models;
+namespace MvcLoginRegistration.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            using (OurDbContext db = new OurDbContext())
+            {
+                return View(db.userAccount.ToList());
+            }
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(UserAccount account)
+        {
+            if (ModelState.IsValid)
+            {
+                using (OurDbContext db = new OurDbContext())
+                {
+                    db.userAccount.Add(account);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+
+                ViewBag.Message= account.FirstName + "" + account.LastName + "Successfully Registered"; 
+            }
+            return View();
+        }
+
+        //login
+        public ActionResult login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult login(UserAccount user)
+        {
+            using (OurDbContext db = new OurDbContext())
+            {
+                var usr = db.userAccount.Single(u => u.UserName == user.UserName && u.Password == user.Password);
+
+                if (usr != null)
+                {
+                    Session["UserID"] = usr.UserID.ToString();
+                    Session["UserName"] = usr.UserName.ToString();
+                    return RedirectToAction("LoggedIn");
+                }
+
+                else
+                {
+                    ModelState.AddModelError("", "UserName or Passwonrd is wrong ");
+                
+                }
+            }
+            return View();
+        }
+        public ActionResult LoggedIn()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+
+            else
+            {
+                return RedirectToAction("LogIn");
+            }
+
+        }
+
+        }
+        
+    }
---
+++ MvcLoginRegistration/MvcLoginRegistration/Models/UserAccount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+ namespace MvcLoginRegistration.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        [Required(ErrorMessage = "FirstName is Required ")]
+        public int UserID { get; set; }
+        [Required(ErrorMessage = "FirstName is Required ")]
+
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName is Required ")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is Required ")]
+
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "UserName is Required ")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is Required ")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+      
+        
+
+        [Compare("Password", ErrorMessage = "Please Confirm your Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword{ get; set; }
+
+
+        
+
+
+    }
+}
